11957f5656566648a77f5e5fec723f551259f613
Faruk Cagan <faruk.cagan@bestera.com.tr>
2024-03-03 01:08:34 +0300

    bug fixed

--- src/components/GenericSearchBar.tsx
+++ src/components/GenericSearchBar.tsx
@@ -1,18 +1,17 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Form, FormControl, InputGroup, Button } from "react-bootstrap";
 
-interface SearchBarProps<T> {
+interface SearchBarProps {
   placeholder?: string;
   onSearch: (query: string) => void;
   filterText: string;
 }
 
-function SearchBar<T>({
+function SearchBar({
   placeholder = "Ara",
   onSearch,
   filterText,
-}: SearchBarProps<T>) {
-  
+}: SearchBarProps) {
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     onSearch(e.target.value);
   };
